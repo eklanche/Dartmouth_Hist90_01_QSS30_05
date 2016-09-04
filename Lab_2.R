d46ec62e49d88e1cfdb61f703ec0e513f8c5e7f3
eklanche <eklanche@umich.edu>
2016-09-04 13:03:46 -0400

    Updated Lab_2.

--- Lab_2.R
+++ Lab_2.R
@@ -43,3 +43,62 @@ e <- d %>% mutate(RACEC=ifelse(RACE==1,'white',
                         ifelse(RACE==5,'Japanese',
                         ifelse(RACE==6,'Other Asian or Pacific Islander','other')))))))
 head(e)
+
+#Read in crosswalk for statefip
+states <- read_csv('./data/statefip.csv')
+
+#Add state names to dataframe
+f <- left_join(e,states,by='STATEFIP')
+head(f)
+
+#Use group_by() and summarise() to determine population by year
+g <- f %>% group_by(YEAR) %>% summarise(NUMBER=sum(PERWT))
+head(g)
+
+#Population by year and sex
+h <- f %>% group_by(YEAR,SEX) %>% summarise(NUMBER=sum(PERWT))
+h
+
+#Population by year and sex
+h <- f %>% group_by(YEAR,SEXF) %>% summarise(NUMBER=sum(PERWT))
+h
+
+#Alternatively
+i <- f %>% group_by(SEXF,YEAR) %>% summarise(NUMBER=sum(PERWT))
+i
+
+#Columns for each gender
+library(tidyr)
+j <- i %>% spread(SEXF,NUMBER)
+j
+
+#Alternatively
+j <- h %>% spread(SEXF,NUMBER)
+j
+
+#Export to .csv
+write_csv(j,'./data/year_sex.csv')
+
+#Population by regions
+#start with data frame f
+head(f)
+#create REGION variable
+k <- f %>% mutate(REGION=ifelse(STATEFIP %in% c(2,15),'Alaska_Hawaii',
+                         ifelse(STATEFIP %in% c(4,6,8,16,30,32,35,41,49,53,56),'West',
+                         ifelse(STATEFIP %in% c(17,18,19,20,26,27,29,31,38,39,46,55),'Midwest',
+                         ifelse(STATEFIP %in% c(9,23,25,33,34,36,42,44,50),'Northeast','South')))))
+head(k)
+#calculate population of each region at each census
+l <- k %>% group_by(YEAR,REGION) %>% summarise(NUMBER=sum(PERWT))
+l
+#spread data frame: row for each year, column for each region
+m <- l %>% spread(REGION,NUMBER)
+m
+#alternatively
+m <- k %>% group_by(YEAR,REGION) %>% summarise(NUMBER=sum(PERWT)) %>% spread(REGION,NUMBER)
+m
+
+#Always exclude Alaska and Hawaii before 1960
+aa <- a %>% filter(!(YEAR < 1960 & STATEFIP %in% c(2,15)))
+aaa <- a %>% filter(YEAR >= 1960 | !(STATEFIP %in% c(2,15)))
+
